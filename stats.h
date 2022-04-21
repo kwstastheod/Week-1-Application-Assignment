1b5ae0b0cfce7b5768c90c4f322c4f66959910cc
Konstantinos Theodosiou <kwstasth@hotmail.com>
2022-04-21 18:27:43 +0300

    Adding code outline and documentation for C1M1 Assessment

--- stats.h
+++ stats.h
@@ -9,32 +9,100 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.h> 
+ * @brief Week 1 Application Assignment
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * In this programming assignment you will create a simple application that performs statistical analytics on a dataset. 
+ * This assignment will help you get re-oriented with c-programming syntax and host machine compilation. We begin by setting up a version control repository on your local machine. You will then develop and test your code there. 
+ * When complete, you will upload a zip of your repository to Coursera. 
+ * Please read through all instructions before starting.
+ * 
+ * @author <Konstantinos Theodosiou>
+ * @date <21/04/2022 >
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
-
+void print_statistics(int median, int mean, int maximum, int minimum);
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief This function prints the statistics of the array calculated in the other functions.
  *
  * <Add Extended Description Here>
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param Median The median of the array.
+ * @param Mean The mean of the array.
+ * @param Maximum The maximum of the array.
+ * @param Minimum The minimum of the array.
  *
- * @return <Add Return Informaiton here>
+ * @return VOID
  */
 
+int find_maximum(unsigned char test[]);
+/**
+ * @brief This function prints the maximum of the array.
+ *
+ * <Add Extended Description Here>
+ *
+ * @param test[] The array to be searched.
+ *
+ * @return maximum The maximum of the array.
+ */
+
+int find_minimum(unsigned char test[]);
+/**
+ * @brief This function prints the minimum of the array.
+ *
+ * <Add Extended Description Here>
+ *
+ * @param test[] The array to be searched.
+ *
+ * @return minimum The minimum of the array.
+ */
+
+int find_mean(unsigned char test[]);
+/**
+ * @brief This function prints the mean of the array.
+ *
+ * <Add Extended Description Here>
+ *
+ * @param test[] The array to be searched.
+ *
+ * @return mean The mean of the array.
+ */
+
+int find_median(unsigned char test[]);
+/**
+ * @brief This function prints the median of the array.
+ *
+ * <Add Extended Description Here>
+ *
+ * @param test[] The array to be searched.
+ *
+ * @return median The median of the array.
+ */
+
+void sort_array(unsigned char test[]);
+/**
+ * @brief This function sorts the array.
+ *
+ * <Add Extended Description Here>
+ *
+ * @param test[] The array to be sorted.
+ *
+ * @return VOID
+ */
+
+void print_array(unsigned char test[]);
+/**
+ * @brief This function prints the array.
+ *
+ * <Add Extended Description Here>
+ *
+ * @param test[] The array to be printed.
+ *
+ * @return VOID
+ */
 
 #endif /* __STATS_H__ */
